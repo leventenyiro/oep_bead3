7f6a5beea52b730aa3731f0710080693701c0c04
Levente Nyírő <nyiro.levente@gmail.com>
2021-05-03 22:22:06 +0200

    add catch.hpp

--- main.cpp
+++ main.cpp
@@ -2,9 +2,23 @@
 
 using namespace std;
 
+#ifdef NORMAL_MODE
+//#define NORMAL_MODE
+
 int main() {
     Simulation test = Simulation("input3.txt");
     test.run(true);
 
     return 0;
 }
+
+#else
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE("no colony", "input0.txt") {
+    Simulation s = Simulation("input0.txt");
+    
+}
+
+#endif
